1dbbcd99274f09993e8ea68a1306337c1ee515a3
amachlou <abderrahim.machlou@gmail.com>
2024-08-05 23:03:53 +0100

    first commit

---
+++ src/main/java/com/amachlou/spring_azure_entraid_oauth2/SpringAzureEntraOauth2Application.java
@@ -0,0 +1,13 @@
+package com.amachlou.spring_azure_entraid_oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureEntraOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureEntraOauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/amachlou/spring_azure_entraid_oauth2/SpringAzureEntraOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amachlou.spring_azure_entraid_oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureEntraOauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
