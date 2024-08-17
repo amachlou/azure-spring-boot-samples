af364c0d6775a57fe843861cb1977e85ea2840e7
amachlou <65126503+amachlou@users.noreply.github.com>
2024-08-17 23:07:00 +0100

    Implementation/spring azure security keyvault secrets (#2)
    * Initial POM
    
    * Stable POM
    
    * track stable files
    
    * config setup

---
+++ spring-azure-security-keyvault-secrets/src/main/java/amachlou/spring_azure_security_keyvault_secrets/KeyVaultProperties.java
@@ -0,0 +1,18 @@
+package amachlou.spring_azure_security_keyvault_secrets;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter @Setter @AllArgsConstructor
+@ConfigurationProperties("azure.keyvault")
+public class KeyVaultProperties {
+    private String vaultUrl;
+    private String tenantId;
+    private String clientId;
+    private String clientSecret;
+
+
+
+}
---
+++ spring-azure-security-keyvault-secrets/src/test/java/amachlou/spring_azure_security_keyvault_secrets/SpringAzureSecurityKeyvaultSecretsApplicationTests.java
@@ -0,0 +1,14 @@
+package amachlou.spring_azure_security_keyvault_secrets;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureSecurityKeyvaultSecretsApplicationTests {
+
+	 @Test
+	 void contextLoads() {
+	 }
+
+}
